0e2528a5ea0bbe2068f3682e70e489cd97e877d9
Голдырев Евгений Алексеевич <Goldyrev.E.Al@sberbank.ru>
2020-06-03 23:26:20 +0300

    improves some mistakes via change spring version

--- app/src/main/java/com/goldyrev/gcloud/GcloudApplication.java
+++ app/src/main/java/com/goldyrev/gcloud/GcloudApplication.java
@@ -2,8 +2,11 @@ package com.goldyrev.gcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GcloudApplication {
     public static void main(String[] args) {
         SpringApplication.run(GcloudApplication.class, args);
--- app/src/main/java/com/goldyrev/gcloud/services/StudentService.java
+++ app/src/main/java/com/goldyrev/gcloud/services/StudentService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@Configuration
 public class StudentService {
 
     @Autowired
