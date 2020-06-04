10176402d64d4ad02940b09c308741916547020c
Голдырев Евгений Алексеевич <Goldyrev.E.Al@sberbank.ru>
2020-06-04 04:23:52 +0300

    return old version

--- app/src/main/java/com/goldyrev/gcloud/controllers/StudentController.java
+++ app/src/main/java/com/goldyrev/gcloud/controllers/StudentController.java
@@ -22,38 +22,37 @@ public class StudentController {
     }
 
     @GetMapping("students/all")
-    public ResponseEntity<List<StudentDTO>> allStudents(){
+    public ResponseEntity<List<StudentDTO>> allStudents() {
         List<StudentDTO> allStudents = studentService.getAllStudents();
         return new ResponseEntity<>(allStudents, OK);
     }
 
     @GetMapping("students/{id}")
-    public ResponseEntity<StudentDTO> studentById(@PathVariable("id") Long id){
-        StudentDTO studentById =  studentService.getStudentById(id);
+    public ResponseEntity<StudentDTO> studentById(@PathVariable("id") Long id) {
+        StudentDTO studentById = studentService.getStudentById(id);
         return new ResponseEntity<>(studentById, OK);
     }
 
     @GetMapping("student/all/{group}")
-    public ResponseEntity<List<StudentDTO>> allByStudentGroupCode(@PathVariable("group") String group){
+    public ResponseEntity<List<StudentDTO>> allByStudentGroupCode(@PathVariable("group") String group) {
         List<StudentDTO> allByStudentGroup = studentService.getAllStudentsByGroupId(group);
         return new ResponseEntity<>(allByStudentGroup, OK);
     }
 
     @DeleteMapping("student/delete/{id}")
-    public void deleteStudentById(@PathVariable("id") Long id){
-       studentService.deleteStudentByPersonalId(id);
+    public void deleteStudentById(@PathVariable("id") Long id) {
+        studentService.deleteStudentByPersonalId(id);
     }
 
-    @PostMapping(path = "student/add/{id}/{studentName}/{studentSecondName}/{studentGroupCode}")
-//    @PostMapping(path = "student/add/")
-//    public ResponseEntity<StudentDTO> addStudent(@RequestBody StudentDTO studentDTO){
-//        return new ResponseEntity<>(studentService.addStudent(studentDTO), OK);
-//    }
-    public ResponseEntity<StudentDTO> addStudent(@PathVariable("id") Long studentPersonalId,
-                                                 @PathVariable("studentName") String studentName,
-                                                 @PathVariable("studentSecondName") String studentSecondName,
-                                                 @PathVariable("studentGroupCode") String studentGroupCode){
-        StudentDTO studentDTO = new StudentDTO(studentPersonalId, studentName, studentSecondName, studentGroupCode);
+    @PostMapping(path = "student/add/")
+    public ResponseEntity<StudentDTO> addStudent(@RequestBody StudentDTO studentDTO) {
         return new ResponseEntity<>(studentService.addStudent(studentDTO), OK);
     }
+//    public ResponseEntity<StudentDTO> addStudent(@RequestParam("id") Long studentPersonalId,
+//                                                 @RequestParam("studentName") String studentName,
+//                                                 @RequestParam("studentSecondName") String studentSecondName,
+//                                                 @RequestParam("studentGroupCode") String studentGroupCode){
+//        StudentDTO studentDTO = new StudentDTO(studentPersonalId, studentName, studentSecondName, studentGroupCode);
+//        return new ResponseEntity<>(studentService.addStudent(studentDTO), OK);
+//  }
 }
